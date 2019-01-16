174ee4204eff9901ef904ba053e8bac96a086902
Ricardo Garcia <ricardoaugustogarcia@hotmail.com>
2019-01-16 14:19:38 -0200

    Ajustes nos modelos, criação dos schemas do GraphQL

--- src/models/team.js
+++ src/models/team.js
@@ -1,16 +1,23 @@
 export default (sequelize, DataTypes) => {
-  const Team = sequelize.define("team", {
-    name: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false
-    }
-  });
+  const Team = sequelize.define(
+    "team",
+    {
+      name: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      }
+    },
+    { underscored: true }
+  );
 
   Team.associate = models => {
     Team.belongsToMany(models.User, {
       through: "member",
-      foreignKey: "teamId"
+      foreignKey: {
+        name: "teamId",
+        field: "team_id"
+      }
     });
     Team.belongsTo(models.User, {
       foreignKey: "owner"
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,39 @@
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define("user", {
-    username: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false
+  const User = sequelize.define(
+    "user",
+    {
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
     },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  });
+    { underscored: true }
+  );
 
   User.associate = models => {
     User.belongsToMany(models.Team, {
       through: "member",
-      foreignKey: "userId"
+      foreignKey: {
+        name: "userId",
+        field: "user_id"
+      }
+    });
+    User.belongsToMany(models.Team, {
+      through: "channel_member",
+      foreignKey: {
+        name: "userId",
+        field: "user_id"
+      }
     });
   };
 
